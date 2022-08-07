b0c0dfed863aefe948315eb0489d11ca814c0657
Caique Brener <Caique.Brener-ext@ab-Inbev.com>
2022-08-07 11:35:30 -0300

    Testando comportamentos do Auth

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -1,8 +1,10 @@
 package br.com.alura.aluraesporte.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
@@ -27,19 +29,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         auth = Firebase.auth
-        auth.createUserWithEmailAndPassword("caique@testeauth.com","1234567890")
+//        registerUser(auth)
+        authUser(auth, "caique.brener@testeauth.com", "12345678")
+        verifyUser(auth)
+        logOff(auth)
+
         controlador.addOnDestinationChangedListener { _,
                                                       destination,
                                                       _ ->
             title = destination.label
             viewModel.componentes.observe(this, Observer {
                 it?.let { temComponentes ->
-                    if(temComponentes.appBar){
+                    if (temComponentes.appBar) {
                         supportActionBar?.show()
                     } else {
                         supportActionBar?.hide()
                     }
-                    if(temComponentes.bottomNavigation) {
+                    if (temComponentes.bottomNavigation) {
                         main_activity_bottom_navigation.visibility = VISIBLE
                     } else {
                         main_activity_bottom_navigation.visibility = GONE
@@ -49,6 +55,37 @@ class MainActivity : AppCompatActivity() {
         }
         main_activity_bottom_navigation
             .setupWithNavController(controlador)
+    }
+
+    private fun logOff(auth:FirebaseAuth) {
+        auth.signOut()
+    }
+
+    private fun verifyUser(auth:FirebaseAuth) {
+        auth.currentUser.let {
+            Toast.makeText(this, "Usuário logado como: ${it?.email}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun authUser(auth: FirebaseAuth, email: String, password: String): Unit {
+        var toastBar: Unit = Toast.makeText(this, "Erro", Toast.LENGTH_SHORT).show()
+        val signInWithEmailAndPassword = auth.signInWithEmailAndPassword(email, password)
+        signInWithEmailAndPassword.addOnSuccessListener {
+            toastBar = Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+
+        }
+        return toastBar
+    }
+
+    private fun registerUser(auth: FirebaseAuth) {
+        val taskResultCreateUser =
+            auth.createUserWithEmailAndPassword("caique.brener@testeauth.com", "12345678")
+        taskResultCreateUser.addOnSuccessListener { AuthResultSuccess ->
+            Log.w("TAG", "$AuthResultSuccess")
+        }
+        taskResultCreateUser.addOnFailureListener { AuthResultFailure ->
+            Log.w("TAG", "$AuthResultFailure")
+        }
     }
 
 }
